8dd2b6920a0c8dcbfc124e4b9fe2979b0d41adde
Olex-Right <sanokoo.its.me@gmail.com>
2024-06-21 08:06:46 +0200

    styled sidebar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,18 +9,20 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
-      <div className="logo">Logo</div>
-      <ul className="menu">
-        {menuItems.map((menuItem) => (
-          <li key={menuItem.link}>
-            <Link to={menuItem.link} className="menu-item">
-              {menuItem.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      {children}
+    <div className="layout">
+      <div className="sidebar">
+        <div className="logo">3Lagoda</div>
+        <ul className="menu">
+          {menuItems.map((menuItem) => (
+            <li key={menuItem.link}>
+              <Link to={menuItem.link} className="menu-item">
+                {menuItem.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="content">{children}</div>
     </div>
   );
 };
