2b96613d7dc39ba17a7969a05c30ffcabfbd31b2
Olex-Right <sanokoo.its.me@gmail.com>
2024-06-23 22:42:19 +0200

    updated routing

--- src/constatns/menuItems.ts
+++ src/constatns/menuItems.ts
@@ -1,14 +1,20 @@
+import Category from "../pages/Category/Category";
+import CategoryRequest1 from "../pages/Category/CategoryRequest1";
+import CategoryRequest2 from "../pages/Category/CategoryRequest2";
+import Employee from "../pages/Employee/Employee";
+
 interface MenuItem {
   name: string;
   link: string;
+  component: () =>  JSX.Element;
 }
 
 const menuItems: MenuItem[] = [
-  { name: "Main", link: "/" },
-  { name: "Category", link: "/category" },
-  { name: "Employee", link: "/employee" },
-  { name: "Categories Request1", link: "/category-request1" },
-  { name: "Categories Request2", link: "/category-request2" },
+  { name: "Main", link: "/", component: Employee },
+  { name: "Category", link: "/category", component: Category   },
+  { name: "Employee", link: "/employee", component: Employee },
+  { name: "Categories Request1", link: "/category-request1", component: CategoryRequest1 },
+  { name: "Categories Request2", link: "/category-request2", component: CategoryRequest2 },
 ];
 
 export default menuItems;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Category from "../pages/Category/Category";
-import Employee from "../pages/Employee/Employee";
-import CategoryRequest1 from "../pages/Category/CategoryRequest1";
-import CategoryRequest2 from "../pages/Category/CategoryRequest2";
+import menuItems from "../constatns/menuItems";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/category-request1" element={<CategoryRequest1 />} />
-      <Route path="/category-request2" element={<CategoryRequest2 />} />
-      <Route path="/category" element={<Category />} />
-      <Route path="/employee" element={<Employee />} />
-      <Route path="/" element={<Employee />} />
+      {menuItems.map((menuItem) => {
+        return <Route key={menuItem.link} path={menuItem.link} element={<menuItem.component/>} />;
+      })}
     </Routes>
   );
 };
